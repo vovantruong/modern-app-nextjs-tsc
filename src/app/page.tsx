7cd850486df6b1aa795feae718a883a2894cb02f
Vo Van Truong <vvtruong1007@gmail.com>
2023-03-27 14:43:36 +0700

    commit setup

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+import Paragraph from './components/ui/Paragraph'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+    return (
+        <main className='bg-red-500 text-white font-medium'>
+            <Paragraph size="sm" >
+                Text
+            </Paragraph>
+        </main>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}",
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
